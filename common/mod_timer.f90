4bd4ae0d23e168f1362e9845463ce810f8e30a8d
NaoMatch <mn1491625@gmail.com>
2021-01-08 20:30:38 +0900

    put comment of file and functions

--- common/mod_timer.f90
+++ common/mod_timer.f90
@@ -1,3 +1,4 @@
+!> A module for timer.
 module mod_timer
     implicit none
 
@@ -8,8 +9,12 @@ module mod_timer
     ! call date_and_time(values=date_value2)
     ! print*, time_diff(date_value1, date_value2)
     ! -------------------------------------------------
+
 contains
 
+    !> A function to get datetime values
+    !! \return returns datetime values
+    !! \param date_value datetime values
     function datetime2second(date_value)
         implicit none
         integer(kind=8) :: date_value(8)
@@ -20,6 +25,10 @@ contains
         datetime2second = datetime2second + date_value(8)         ! Add milliseconds
     end function datetime2second
 
+    !> A function to compute time difference
+    !! \return returns time difference
+    !! \param date_value1 datetime value at time 1
+    !! \param date_value1 datetime value at time 2
     function time_diff(date_value1, date_value2)
         implicit none
         integer(kind=8) :: date_value1(8), date_value2(8)
