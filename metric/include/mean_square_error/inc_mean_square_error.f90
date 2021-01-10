98052d26a0e20630175e680e55b81a5faccca187
NaoMatch <mn1491625@gmail.com>
2021-01-10 23:42:08 +0900

    add sample script main_metric.f90, modify mod_metric.f90, and update makefile

--- metric/include/mean_square_error/inc_mean_square_error.f90
+++ metric/include/mean_square_error/inc_mean_square_error.f90
@@ -1,61 +1,42 @@
-function mean_square_error_real64(y_true, y_pred)
+function mean_square_error_r8(this, y_true, y_pred)
     implicit none
+    class(metrics)           :: this
     real(kind=8), intent(in) :: y_true(:)
     real(kind=8), intent(in) :: y_pred(:)
-    real(kind=8) :: mean_square_error_real64
+    real(kind=8) :: mean_square_error_r8
 
     real(kind=8) :: tmp_sq_sum
-    integer(kind=8) :: i, num_y_true, num_y_pred
+    integer(kind=8) :: i, y_size_true, y_size_pred
 
     include "./include/mean_square_error/inc_mean_square_error_detail.f90"
-    mean_square_error_real64 = tmp_sq_sum / dble(num_y_true)
-end function mean_square_error_real64
+    mean_square_error_r8 = tmp_sq_sum / dble(y_size_true)
+end function mean_square_error_r8
 
-function mean_square_error_int32(y_true, y_pred)
+function mean_square_error_i4(this, y_true, y_pred)
     implicit none
+    class(metrics)           :: this
     real(kind=4), intent(in) :: y_true(:)
     integer(kind=4), intent(in) :: y_pred(:)
-    real(kind=4) :: mean_square_error_int32
+    real(kind=4) :: mean_square_error_i4
 
     real(kind=4) :: tmp_sq_sum
-    integer(kind=4) :: i, num_y_true, num_y_pred
+    integer(kind=4) :: i, y_size_true, y_size_pred
 
     include "./include/mean_square_error/inc_mean_square_error_detail.f90"
-    mean_square_error_int32 = tmp_sq_sum / dble(num_y_true)
-end function mean_square_error_int32
+    mean_square_error_i4 = tmp_sq_sum / dble(y_size_true)
+end function mean_square_error_i4
 
-function mean_square_error_int64(y_true, y_pred)
+function mean_square_error_i8(this, y_true, y_pred)
     implicit none
+    class(metrics)           :: this
     real(kind=8), intent(in) :: y_true(:)
     integer(kind=8), intent(in) :: y_pred(:)
-    real(kind=8) :: mean_square_error_int64
+    real(kind=8) :: mean_square_error_i8
 
     real(kind=8) :: tmp_sq_sum
-    integer(kind=8) :: i, num_y_true, num_y_pred
+    integer(kind=8) :: i, y_size_true, y_size_pred
 
     include "./include/mean_square_error/inc_mean_square_error_detail.f90"
-    mean_square_error_int64 = tmp_sq_sum / dble(num_y_true)
-end function mean_square_error_int64
+    mean_square_error_i8 = tmp_sq_sum / dble(y_size_true)
+end function mean_square_error_i8
 
-function mean_square_error_r8_r8_mat(y_true, y_pred)
-    implicit none
-    real(kind=8), intent(in) :: y_true(:,:)
-    real(kind=8), intent(in) :: y_pred(:,:)
-    real(kind=8) :: mean_square_error_r8_r8_mat
-
-    real(kind=8) :: tmp_sq_sum
-    integer(kind=8) :: i, num_y_true, num_y_pred, n_outpts
-
-    n_outpts = size(y_true(1,:))
-    num_y_true = size(y_true(:,1))
-    num_y_pred = size(y_pred(:,1))
-    if (num_y_true .ne. num_y_pred) then
-        stop "Size of y_true and y_pred missmatch."
-    end if
-
-    tmp_sq_sum = 0
-    do i=1, num_y_true, 1
-        tmp_sq_sum = tmp_sq_sum + sum((y_true(i,:)-y_pred(i,:)) ** 2.0)
-    end do        
-    mean_square_error_r8_r8_mat = tmp_sq_sum / dble(num_y_true) / dble(n_outpts)
-end function mean_square_error_r8_r8_mat
--- metric/include/root_mean_square_error/inc_root_mean_square_error.f90
+++ metric/include/root_mean_square_error/inc_root_mean_square_error.f90
@@ -1,24 +1,27 @@
-function root_mean_square_error_real64(y_true, y_pred)
+function root_mean_square_error_r8(this, y_true, y_pred)
     implicit none
+    class(metrics)           :: this
     real(kind=8), intent(in) :: y_true(:)
     real(kind=8), intent(in) :: y_pred(:)
-    real(kind=8) :: root_mean_square_error_real64
-    root_mean_square_error_real64 = sqrt(mean_square_error_real64(y_true, y_pred))
-end function root_mean_square_error_real64
+    real(kind=8) :: root_mean_square_error_r8
+    root_mean_square_error_r8 = sqrt(this%mean_square_error_r8(y_true, y_pred))
+end function root_mean_square_error_r8
 
-function root_mean_square_error_int32(y_true, y_pred)
+function root_mean_square_error_i4(this, y_true, y_pred)
     implicit none
+    class(metrics)           :: this
     real(kind=4), intent(in) :: y_true(:)
     integer(kind=4), intent(in) :: y_pred(:)
-    real(kind=4) :: root_mean_square_error_int32
-    root_mean_square_error_int32 = sqrt(mean_square_error_int32(y_true, y_pred))
-end function root_mean_square_error_int32
+    real(kind=4) :: root_mean_square_error_i4
+    root_mean_square_error_i4 = sqrt(this%mean_square_error_i4(y_true, y_pred))
+end function root_mean_square_error_i4
 
-function root_mean_square_error_int64(y_true, y_pred)
+function root_mean_square_error_i8(this, y_true, y_pred)
     implicit none
+    class(metrics)           :: this
     real(kind=8), intent(in) :: y_true(:)
     integer(kind=8), intent(in) :: y_pred(:)
-    real(kind=8) :: root_mean_square_error_int64
-    root_mean_square_error_int64 = sqrt(mean_square_error_int64(y_true, y_pred))
-end function root_mean_square_error_int64
+    real(kind=8) :: root_mean_square_error_i8
+    root_mean_square_error_i8 = sqrt(this%mean_square_error_i8(y_true, y_pred))
+end function root_mean_square_error_i8
 
