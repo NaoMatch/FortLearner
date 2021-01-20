1c46f947f05bea2e937054c8c0fde4bb129c45da
NaoMatch <mn1491625@gmail.com>
2021-01-20 23:15:11 +0900

    add error module and shape of x and y

--- data_holder/include/new_data_holder/inc_new_data_holder.f90
+++ data_holder/include/new_data_holder/inc_new_data_holder.f90
@@ -5,13 +5,17 @@ function new_data_holder_r8_i8(x, y)
     integer(kind=8), target :: y(:,:)
 
     integer(kind=8) :: x_shape(2), y_shape(2)
-
+    type(error)     :: err
+    
     x_shape = shape(x)
     y_shape = shape(y)
+    call err % sample_size_mismatch(x_shape, "x", y_shape, "y", "data_holder")
 
     new_data_holder_r8_i8 % n_outputs         =  y_shape(2)
     new_data_holder_r8_i8 % n_samples         =  x_shape(1)
     new_data_holder_r8_i8 % n_columns         =  x_shape(2)
+    new_data_holder_r8_i8 % x_shape           =  x_shape
+    new_data_holder_r8_i8 % y_shape           =  y_shape
     new_data_holder_r8_i8 % x_ptr % x_r8_ptr  => x
     new_data_holder_r8_i8 % y_ptr % y_i8_ptr  => y
 
