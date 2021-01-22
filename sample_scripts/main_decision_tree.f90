86a19e23588c7ce3c0e2624dcdd8a7f05206faac
NaoMatch <mn1491625@gmail.com>
2021-01-22 19:38:28 +0900

    add extra_tree_regressor

--- sample_scripts/main_decision_tree.f90
+++ sample_scripts/main_decision_tree.f90
@@ -4,9 +4,10 @@ program main_decision_tree
     use mod_metric
     use mod_data_holder
     use mod_decision_tree
+    use mod_extra_tree
     implicit none
 
-    integer(kind=8) :: date_value1(8), date_value2(8)
+    integer(kind=8) :: date_value1(8), date_value2(8), time_dt, time_et
 
     integer(kind=8)    :: n_samples_train, n_columns_train
     integer(kind=8)    :: n_samples_test, n_columns_test
@@ -18,13 +19,14 @@ program main_decision_tree
     CHARACTER(len=256) :: file_name_x_test_bin, file_name_y_test_bin
     real(kind=8), ALLOCATABLE :: x_train(:,:), x_test(:,:)
     real(kind=8), ALLOCATABLE :: y_train(:,:), y_test(:,:)
-    real(kind=8), ALLOCATABLE :: y_train_pred(:,:)
-    real(kind=8), ALLOCATABLE :: y_test_pred(:,:)
+    real(kind=8), ALLOCATABLE :: y_train_pred_dt(:,:), y_test_pred_dt(:,:)
+    real(kind=8), ALLOCATABLE :: y_train_pred_et(:,:), y_test_pred_et(:,:)
     
     type(metrics)                 :: metric
     type(data_holder), target     :: dholder
     type(data_holder), pointer    :: dholder_ptr
     type(decision_tree_regressor) :: dt_reg
+    type(extra_tree_regressor)    :: et_reg
     integer(kind=8)    :: i, n_leaf_nodes, max_leaf_nodes
     integer(kind=8)    :: iter, max_iter
 
@@ -32,10 +34,10 @@ program main_decision_tree
 
     print*, '============================================================='
     print*, "Input Data Shape: "
-    n_samples_train  = 824_8
-    n_samples_test   = 103_8
-    n_columns_train  = 8_8
-    n_columns_test   = 8_8
+    n_samples_train  = 7654_8
+    n_samples_test   = 957_8
+    n_columns_train  = 4_8
+    n_columns_test   = 4_8
     skip_header = t_
     dtype_in = "r"
     dtype_out = "r"
@@ -47,18 +49,49 @@ program main_decision_tree
 
     print*, '============================================================='
     print*, "File Names: "
-    file_name_x_train_csv = "./input/Concrete_x_train.csv"
-    file_name_y_train_csv = "./input/Concrete_y_train.csv"
-    file_name_x_train_bin = "./input/Concrete_x_train.bin"
-    file_name_y_train_bin = "./input/Concrete_y_train.bin"
-    file_name_x_test_csv  = "./input/Concrete_x_test.csv"
-    file_name_y_test_csv  = "./input/Concrete_y_test.csv"
-    file_name_x_test_bin  = "./input/Concrete_x_test.bin"
-    file_name_y_test_bin  = "./input/Concrete_y_test.bin"
+    file_name_x_train_csv = "./input/Combined_Cycle_x_train.csv"
+    file_name_y_train_csv = "./input/Combined_Cycle_y_train.csv"
+    file_name_x_train_bin = "./input/Combined_Cycle_x_train.bin"
+    file_name_y_train_bin = "./input/Combined_Cycle_y_train.bin"
+    file_name_x_test_csv  = "./input/Combined_Cycle_x_test.csv"
+    file_name_y_test_csv  = "./input/Combined_Cycle_y_test.csv"
+    file_name_x_test_bin  = "./input/Combined_Cycle_x_test.bin"
+    file_name_y_test_bin  = "./input/Combined_Cycle_y_test.bin"
     print*, "   x_train csv -> bin: ", trim(file_name_x_train_csv), " -> ", trim(file_name_x_train_bin)
     print*, "   y_train csv -> bin: ", trim(file_name_y_train_csv), " -> ", trim(file_name_y_train_bin)
     print*, "   x_test  csv -> bin: ", trim(file_name_x_test_csv),  " -> ", trim(file_name_x_test_bin)
     print*, "   y_test  csv -> bin: ", trim(file_name_y_test_csv),  " -> ", trim(file_name_y_test_bin)
+
+
+    ! print*, '============================================================='
+    ! print*, "Input Data Shape: "
+    ! n_samples_train  = 824_8
+    ! n_samples_test   = 103_8
+    ! n_columns_train  = 8_8
+    ! n_columns_test   = 8_8
+    ! skip_header = t_
+    ! dtype_in = "r"
+    ! dtype_out = "r"
+    ! print*, "    train (row x col): ", n_samples_train, n_columns_train
+    ! print*, "    test  (row x col): ", n_samples_test, n_columns_test
+    ! print*, "    skip header:       ", skip_header
+    ! print*, "    data type input:   ", dtype_in
+    ! print*, "    data type output:  ", dtype_out
+
+    ! print*, '============================================================='
+    ! print*, "File Names: "
+    ! file_name_x_train_csv = "./input/Concrete_x_train.csv"
+    ! file_name_y_train_csv = "./input/Concrete_y_train.csv"
+    ! file_name_x_train_bin = "./input/Concrete_x_train.bin"
+    ! file_name_y_train_bin = "./input/Concrete_y_train.bin"
+    ! file_name_x_test_csv  = "./input/Concrete_x_test.csv"
+    ! file_name_y_test_csv  = "./input/Concrete_y_test.csv"
+    ! file_name_x_test_bin  = "./input/Concrete_x_test.bin"
+    ! file_name_y_test_bin  = "./input/Concrete_y_test.bin"
+    ! print*, "   x_train csv -> bin: ", trim(file_name_x_train_csv), " -> ", trim(file_name_x_train_bin)
+    ! print*, "   y_train csv -> bin: ", trim(file_name_y_train_csv), " -> ", trim(file_name_y_train_bin)
+    ! print*, "   x_test  csv -> bin: ", trim(file_name_x_test_csv),  " -> ", trim(file_name_x_test_bin)
+    ! print*, "   y_test  csv -> bin: ", trim(file_name_y_test_csv),  " -> ", trim(file_name_y_test_bin)
 
     print*, '============================================================='
     print*, "CSV to Binary"
@@ -92,8 +125,8 @@ program main_decision_tree
     dholder = data_holder(x_train, y_train)
     dholder_ptr => dholder
 
-    max_iter = 100
-    max_leaf_nodes = 100
+    max_iter = 20
+    max_leaf_nodes = 20
     do n_leaf_nodes=2, max_leaf_nodes
         dt_reg = decision_tree_regressor(max_leaf_nodes=n_leaf_nodes, fashion="best")
         call date_and_time(values=date_value1)
@@ -101,11 +134,29 @@ program main_decision_tree
             call dt_reg%fit(dholder_ptr)
         end do
         call date_and_time(values=date_value2)
-        y_train_pred = dt_reg%predict(x_train)
-        y_test_pred = dt_reg%predict(x_test)
+        y_train_pred_dt = dt_reg%predict(x_train)
+        y_test_pred_dt = dt_reg%predict(x_test)
+        time_dt = time_diff(date_value1, date_value2)
+
+
+        et_reg = extra_tree_regressor(max_leaf_nodes=n_leaf_nodes, fashion="best", n_repeats=1_8)
+        call date_and_time(values=date_value1)
+        do iter=1, max_iter, 1
+            call et_reg%fit(dholder_ptr)
+        end do
+        call date_and_time(values=date_value2)
+        y_train_pred_et = et_reg%predict(x_train)
+        y_test_pred_et = et_reg%predict(x_test)
+        time_et = time_diff(date_value1, date_value2)
+
         print*, "    mse train vs test: ", n_leaf_nodes, &
-            metric%mean_square_error(y_train(:,1), y_train_pred(:,1)), &
-            metric%mean_square_error(y_test(:,1), y_test_pred(:,1)), &
-            time_diff(date_value1, date_value2)/dble(max_iter), "[msec]"
+            ! print*, '============================================================='
+            real(metric%mean_square_error(y_train(:,1), y_train_pred_dt(:,1))), &
+            real(metric%mean_square_error(y_test(:,1), y_test_pred_dt(:,1))), &
+            real(time_dt/dble(max_iter)), "[msec]", &
+            ! print*, '============================================================='
+            real(metric%mean_square_error(y_train(:,1), y_train_pred_et(:,1))), &
+            real(metric%mean_square_error(y_test(:,1), y_test_pred_et(:,1))), &
+            real(time_et/dble(max_iter)), "[msec]"
     end do
 end program main_decision_tree
