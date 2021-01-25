e2ec58a68ab1fdbfa9e44fb431bd63aef766ac97
NaoMatch <mn1491625@gmail.com>
2021-01-25 23:18:56 +0900

    add hyperparameter type for sgd_regressor

--- hyperparameter/mod_hyperparameter.f90
+++ hyperparameter/mod_hyperparameter.f90
@@ -89,6 +89,25 @@ module mod_hyperparameter
         character(len=256) :: fashion               = "best" !> best, depth, level, impurity, sample
     end type hparam_decisiontree
 
+    type, extends(hparam_base) :: hparam_sgd_estimator
+        character(len=256) :: loss = "squared_loss"
+        character(len=256) :: penalty = "l2"
+        real(kind=8)       :: alpha = 0.0001d0
+        real(kind=8)       :: l1_ratio = 0.15d0
+        logical(kind=4)    :: fit_intercept = t_
+        integer(kind=8)    :: max_iter=1000_8
+        real(kind=8)       :: tolerance = 0.0001d0
+        logical(kind=4)    :: shuffle = t_
+        integer(kind=4)    :: verbose = 0_8
+        real(kind=8)       :: epsilon = 0.1d0
+        character(len=256) :: learning_rate = "constant"
+        real(kind=8)       :: learning_rate_initial = 0.01d0
+        real(kind=8)       :: power_t = 0.25d0
+        logical(kind=4)    :: early_stopping = f_
+        real(kind=8)       :: validation_fraction = 0.01d0
+        integer(kind=8)    :: n_iter_no_change=10_8
+    end type hparam_sgd_estimator
+
 contains
 
     !> A subroutine to validate character hyperparameter.
