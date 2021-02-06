6dc8364b9819366493a2a05893b3d96fb32a7ec7
NaoMatch <mn1491625@gmail.com>
2021-02-06 11:16:40 +0900

    add new input type

--- data_holder/include/new_data_holder/inc_new_data_holder.f90
+++ data_holder/include/new_data_holder/inc_new_data_holder.f90
@@ -21,3 +21,27 @@ function new_data_holder_r8_i8(x, y)
 
     new_data_holder_r8_i8 % is_preprocessed = f_
 end function new_data_holder_r8_i8
+
+function new_data_holder_i4_r4(x, y)
+    implicit none
+    type(data_holder)       :: new_data_holder_i4_r4
+    integer(kind=4), target :: x(:,:)
+    real(kind=4), target    :: y(:,:)
+
+    integer(kind=8) :: x_shape(2), y_shape(2)
+    type(error)     :: err
+
+    x_shape = shape(x)
+    y_shape = shape(y)
+    call err % sample_size_mismatch(x_shape, "x", y_shape, "y", "data_holder")
+
+    new_data_holder_i4_r4 % n_outputs         =  y_shape(2)
+    new_data_holder_i4_r4 % n_samples         =  x_shape(1)
+    new_data_holder_i4_r4 % n_columns         =  x_shape(2)
+    new_data_holder_i4_r4 % x_shape           =  x_shape
+    new_data_holder_i4_r4 % y_shape           =  y_shape
+    new_data_holder_i4_r4 % x_ptr % x_i4_ptr  => x
+    new_data_holder_i4_r4 % y_ptr % y_r4_ptr  => y
+
+    new_data_holder_i4_r4 % is_preprocessed = f_
+end function new_data_holder_i4_r4
