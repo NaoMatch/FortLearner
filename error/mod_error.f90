f476fb0ac15043439c5a146104f0e90e7b7c25f3
NaoMatch <mn1491625@gmail.com>
2021-02-14 16:02:21 +0900

    add is_binary_labels

--- error/mod_error.f90
+++ error/mod_error.f90
@@ -5,13 +5,19 @@ module mod_error
     type error
     contains
         procedure :: check_estimator_is_fitted
+        procedure :: only_accept_Nx1_matrix
+
         procedure, pass :: check_number_of_features_mismatch_i4
         procedure, pass :: check_number_of_features_mismatch_i8
         generic :: check_number_of_features_mismatch => check_number_of_features_mismatch_i4, check_number_of_features_mismatch_i8
-        procedure :: only_accept_Nx1_matrix
+
         procedure, pass :: sample_size_mismatch_matrix
         procedure, pass :: sample_size_mismatch_vector
         generic :: sample_size_mismatch => sample_size_mismatch_vector, sample_size_mismatch_matrix
+
+        procedure, pass :: is_binary_labels_i4
+        procedure, pass :: is_binary_labels_i8
+        generic :: is_binary_labels => is_binary_labels_i4, is_binary_labels_i8
     end type
 
 contains
@@ -109,5 +115,53 @@ contains
             stop trim(err_msg)
         end if
     end subroutine sample_size_mismatch_matrix
+
+    subroutine is_binary_labels_i4(this, labels, n_samples, function_name)
+        class(error)     :: this
+        integer(kind=4)  :: labels(n_samples, 1_4)
+        integer(kind=4)  :: n_samples
+        character(len=*) :: function_name
+
+        integer(kind=4)    :: n
+        integer(kind=4)    :: min, max, val
+        character(len=512) :: err_msg
+
+        min =  huge(0_4)
+        max = -huge(0_4)
+        
+        do n=1, n_samples, 1
+            val = labels(n,1)
+            min = minval((/min, val/))
+            max = maxval((/max, val/))
+        end do
+        if (min .eq. 0_4 .and. max .eq. 1_4) return
+
+        err_msg = "ValueError: The intput labels must be 0 and 1 for " // trim(function_name) // "."
+        stop trim(err_msg)
+    end subroutine is_binary_labels_i4
+
+    subroutine is_binary_labels_i8(this, labels, n_samples, function_name)
+        class(error)     :: this
+        integer(kind=8)  :: labels(n_samples, 1_8)
+        integer(kind=8)  :: n_samples
+        character(len=*) :: function_name
+
+        integer(kind=8)    :: n
+        integer(kind=8)    :: min, max, val
+        character(len=512) :: err_msg
+
+        min =  huge(0_8)
+        max = -huge(0_8)
+        
+        do n=1, n_samples, 1
+            val = labels(n,1)
+            min = minval((/min, val/))
+            max = maxval((/max, val/))
+        end do
+        if (min .eq. 0_8 .and. max .eq. 1_8) return
+
+        err_msg = "ValueError: The intput labels must be 0 and 1 for " // trim(function_name) // "."
+        stop trim(err_msg)
+    end subroutine is_binary_labels_i8
 
 end module mod_error
