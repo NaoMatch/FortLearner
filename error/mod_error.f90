b98f99df08f7e886c8bdb4cb04d984906a97d900
NaoMatch <mn1491625@gmail.com>
2021-03-15 23:00:27 +0900

    add comments and move to include file

--- error/mod_error.f90
+++ error/mod_error.f90
@@ -2,6 +2,7 @@ module mod_error
     use mod_common
     implicit none
     
+    !> A type for error messages.
     type error
     contains
         procedure :: check_estimator_is_fitted
@@ -22,6 +23,9 @@ module mod_error
 
 contains
 
+    !> A subroutine to check estimator is already fitted
+    !! \param is_trained if true, estimator is fitted.
+    !! \param function_name algorithm name
     subroutine check_estimator_is_fitted(this, is_trained, function_name)
         implicit none
         class(error)     :: this
@@ -37,6 +41,10 @@ contains
     end subroutine check_estimator_is_fitted       
 
 
+    !> A subroutine to make sure that the number of features during training matches the number of features during transforming 
+    !! \param n_columns_train number of features during training
+    !! \param n_columns_transform number of features during transforming
+    !! \param function_name algorithm name
     subroutine check_number_of_features_mismatch_i4(this, n_columns_train, n_columns_transform, function_name)
         class(error)     :: this
         integer(kind=4)  :: n_columns_train, n_columns_transform
@@ -52,25 +60,13 @@ contains
             stop trim(err_msg)
         end if
     end subroutine check_number_of_features_mismatch_i4
+    include "./include/error_check_number_of_features_mismatch/inc_check_number_of_features_mismatch.f90"
 
 
-    subroutine check_number_of_features_mismatch_i8(this, n_columns_train, n_columns_transform, function_name)
-        class(error)     :: this
-        integer(kind=8)  :: n_columns_train, n_columns_transform
-        character(len=*) :: function_name
-        character(len=512) :: err_msg
-        character(:), ALLOCATABLE :: char_train, char_transform
-
-        if (n_columns_train .ne. n_columns_transform) then
-            char_train = num2char(n_columns_train)
-            char_transform = num2char(n_columns_transform)
-            err_msg = "ValueError: The number of columns does not match the number of columns during training."
-            err_msg = trim(err_msg) // " " // trim(char_train) // " in Train, " // trim(char_transform) // " in Transform."
-            stop trim(err_msg)
-        end if
-    end subroutine check_number_of_features_mismatch_i8
-
-
+    !> A subroutine to check Nx1 matrix or not
+    !! \param shape_y shape of matrix
+    !! \param variable_name_y variable name of matrix
+    !! \param function_name algorithm name
     subroutine only_accept_Nx1_matrix(this, shape_y, variable_name_y, function_name)
         class(error)     :: this
         integer(kind=8)  :: shape_y(2), one_i=1_8
@@ -86,6 +82,12 @@ contains
     end subroutine only_accept_Nx1_matrix
 
 
+    !> A subroutine to check if the number of samples in two vectors match.
+    !! \param n_samples_x number of samples in vector_x
+    !! \param variable_name_x variable name of vector_x
+    !! \param n_samples_y number of samples in vector_y
+    !! \param variable_name_y variable name of vector_y
+    !! \param function_name algorithm name
     subroutine sample_size_mismatch_vector(this, n_samples_x, variable_name_x, n_samples_y, variable_name_y, function_name)
         implicit none
         class(error)     :: this
@@ -101,6 +103,13 @@ contains
         end if
     end subroutine sample_size_mismatch_vector
 
+
+    !> A subroutine to check if the shapes of the two matrices match.
+    !! \param shape_x shape of matrix_x
+    !! \param variable_name_x variable name of matrix_x
+    !! \param shape_y shape of matrix_y
+    !! \param variable_name_y variable name of matrix_y
+    !! \param function_name algorithm name
     subroutine sample_size_mismatch_matrix(this, shape_x, variable_name_x, shape_y, variable_name_y, function_name)
         implicit none
         class(error)     :: this
@@ -116,6 +125,11 @@ contains
         end if
     end subroutine sample_size_mismatch_matrix
 
+
+    !> A subroutine to check if the input labels are binary only.
+    !! \param labels labels
+    !! \param n_samples number of samples in labels
+    !! \param function_name algorithm name
     subroutine is_binary_labels_i4(this, labels, n_samples, function_name)
         class(error)     :: this
         integer(kind=4)  :: labels(n_samples, 1_4)
@@ -139,29 +153,7 @@ contains
         err_msg = "ValueError: The intput labels must be 0 and 1 for " // trim(function_name) // "."
         stop trim(err_msg)
     end subroutine is_binary_labels_i4
+    include "./include/error_is_binary_labels/inc_is_binary_labels.f90"
 
-    subroutine is_binary_labels_i8(this, labels, n_samples, function_name)
-        class(error)     :: this
-        integer(kind=8)  :: labels(n_samples, 1_8)
-        integer(kind=8)  :: n_samples
-        character(len=*) :: function_name
-
-        integer(kind=8)    :: n
-        integer(kind=8)    :: min, max, val
-        character(len=512) :: err_msg
-
-        min =  huge(0_8)
-        max = -huge(0_8)
-        
-        do n=1, n_samples, 1
-            val = labels(n,1)
-            min = minval((/min, val/))
-            max = maxval((/max, val/))
-        end do
-        if (min .eq. 0_8 .and. max .eq. 1_8) return
-
-        err_msg = "ValueError: The intput labels must be 0 and 1 for " // trim(function_name) // "."
-        stop trim(err_msg)
-    end subroutine is_binary_labels_i8
 
 end module mod_error
