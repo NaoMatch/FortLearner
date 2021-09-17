093a294f24eb931c3d440ab7a1aa68131e88fd1f
NaoMatch <mn1491625@gmail.com>
2021-09-17 23:37:49 +0900

    add sadt_regresser subrouine

--- decision_tree/mod_splitter.f90
+++ decision_tree/mod_splitter.f90
@@ -6,6 +6,7 @@ module mod_splitter
     use mod_sort
     use mod_timer
     use mod_stats
+    use mod_linalg
     use mod_discretizer
     use mod_node
     implicit none
@@ -25,6 +26,9 @@ module mod_splitter
 
         procedure :: split_clouds_regressor
         procedure :: split_clouds_regressor_indivisuals
+
+        procedure :: split_sadt_regressor
+        procedure :: split_sadt_regressor_indivisuals
 
         ! procedure :: split_random_rotation_tree_regressor
         ! procedure :: split_random_rotation_tree_regressor_indivisuals
@@ -48,8 +52,255 @@ module mod_splitter
 
 
 contains
+    !------------------------------------------------------------------------------------------------------------------------------------------ 
+    !------------------------------------------------------------------------------------------------------------------------------------------ 
+    !> A subroutine to split node by extremely randomized way.
+    subroutine split_sadt_regressor(this, node_ptrs, data_holder_ptr, hparam_ptr, & 
+        n_columns, feature_indices, feature_indices_scanning_range, is_permute_per_node)
+        implicit none
+        class(node_splitter)               :: this
+        type(node_oblq_ptr), intent(inout) :: node_ptrs(:)
+        type(data_holder), pointer         :: data_holder_ptr
+        type(hparam_decisiontree), pointer :: hparam_ptr
+        integer(kind=8), intent(in)        :: n_columns
+        integer(kind=8), intent(inout)     :: feature_indices(n_columns)
+        integer(kind=8), intent(in)        :: feature_indices_scanning_range(2)
+        logical(kind=4), intent(in)        :: is_permute_per_node
+        integer(kind=8) :: n
+
+        if ( size(node_ptrs) .eq. 1 ) then
+            call this%split_sadt_regressor_indivisuals(node_ptrs(1)%node_ptr, data_holder_ptr, hparam_ptr, &
+                n_columns, feature_indices, feature_indices_scanning_range, is_permute_per_node)
+        else
+            do n=1, size(node_ptrs), 1
+                call this%split_sadt_regressor_indivisuals(node_ptrs(n)%node_ptr, data_holder_ptr, hparam_ptr, &
+                    n_columns, feature_indices, feature_indices_scanning_range, is_permute_per_node)
+            end do
+        end if
+    end subroutine split_sadt_regressor
+
+    !> A subroutine to split node by extremely randomized way.
+    subroutine split_sadt_regressor_indivisuals(this, node_ptr, data_holder_ptr, hparam_ptr, &
+        n_columns, feature_indices, feature_indices_scanning_range, is_permute_per_node)
+        implicit none
+        class(node_splitter)               :: this
+        type(node_oblq), pointer           :: node_ptr
+        type(data_holder), pointer         :: data_holder_ptr
+        type(hparam_decisiontree), pointer :: hparam_ptr
+        integer(kind=8), intent(in)        :: n_columns
+        integer(kind=8), intent(inout)     :: feature_indices(n_columns)
+        integer(kind=8), intent(in)        :: feature_indices_scanning_range(2)
+        logical(kind=4), intent(in)        :: is_permute_per_node
+
+        real(kind=8) :: n_rows_inv, n_outs_inv
+        real(kind=8), allocatable :: res_l(:), res_r(:), sum_l(:), sum_r(:), avg_l(:), avg_r(:)
+        real(kind=8), allocatable :: tot_p(:), tot_l(:), tot_r(:)
+        real(kind=8) :: gain, gain_best, best_threshold
+        integer(kind=8) :: i_start, i_stop, fid, f, i, j, best_fid, r
+        integer(kind=8) :: count_l, count_r, idx, count_eval
+        integer(kind=8) :: n_samples_l, n_samples_r
+        real(kind=8), allocatable :: tmp_x(:,:), tmp_y(:,:), tmp_f_copy(:), tmp_r(:)
+        integer(kind=8), allocatable :: feature_ids(:), indices(:)
+        integer(kind=8) :: factor
+        integer(kind=8) :: ini_f_idx, fin_f_idx
+        real(kind=8) :: min_val, max_val, rand_val, threshold
+        integer(kind=8), save :: tot_time_minmax=0_8
+        integer(kind=8), save :: tot_time_sum_up=0_8
+        integer(kind=8), save :: tot_time_collect=0_8
+        integer(kind=8) :: date_value1(8), date_value2(8)
+        integer(kind=8) :: e
+        real(kind=8), allocatable :: coefs_current(:)
+        real(kind=8) :: intercept_current
+        real(kind=8), allocatable :: coefs_new(:)
+        real(kind=8) :: intercept_new
+        real(kind=8), allocatable :: coefs_add(:)
+        real(kind=8) :: intercept_add
+        real(kind=8)    :: tmp_avg_l, tmp_avg_r
+        real(kind=8)    :: tmp_sum_l, tmp_sum_r, tot_sum
+        integer(kind=8) :: tmp_cnt_l, tmp_cnt_r, tot_cnt
+        real(kind=8) :: gain_current, gain_new, gain_diff
+        real(kind=8)    :: sum_current_l, sum_current_r
+        real(kind=8)    :: avg_current_l, avg_current_r
+        integer(kind=8) :: cnt_current_l, cnt_current_r
+        real(kind=8)    :: temperature, initial_temperature
+        real(kind=8)    :: transition_proba, proba
+
+        allocate(tot_p(data_holder_ptr%n_outputs))
+        allocate(tot_l(data_holder_ptr%n_outputs))
+        allocate(tot_r(data_holder_ptr%n_outputs))
+        allocate(res_l(data_holder_ptr%n_outputs))
+        allocate(res_r(data_holder_ptr%n_outputs))
+        allocate(sum_l(data_holder_ptr%n_outputs))
+        allocate(sum_r(data_holder_ptr%n_outputs))
+        allocate(avg_l(data_holder_ptr%n_outputs))
+        allocate(avg_r(data_holder_ptr%n_outputs))
+        node_ptr%n_outputs = data_holder_ptr%n_outputs
+
+        gain_best = - huge(0d0)
+        n_rows_inv = 1d0 / dble(node_ptr%n_samples)
+        n_outs_inv = 1d0 / dble(node_ptr%n_outputs)
+        tot_p = node_ptr%sum_p
+        count_eval = 0_8
+        initial_temperature = hparam_ptr % initial_temperature
+        temperature = initial_temperature
+
+        allocate(tmp_y(node_ptr%n_samples, node_ptr%n_outputs))
+        allocate(tmp_x(node_ptr%n_samples, node_ptr%n_columns))
+        allocate(tmp_r(node_ptr%n_samples))
+        allocate(indices(node_ptr%n_samples))
+
+        tot_sum = tot_p(1)
+        tot_cnt = node_ptr%n_samples
+
+        do i=1, node_ptr%n_samples, 1
+            idx = node_ptr%indices(i)
+            tmp_y(i,:) = data_holder_ptr%y_ptr%y_r8_ptr(idx,:)
+            tmp_x(i,:) = data_holder_ptr%x_ptr%x_r8_ptr(idx,:)
+        end do
+
+        allocate(coefs_current(node_ptr%n_columns), coefs_new(node_ptr%n_columns), coefs_add(node_ptr%n_columns))
+        call random_number(coefs_current)
+        call random_number(intercept_current)
+        coefs_current     = 2d0*coefs_current     - 1d0
+        intercept_current = 2d0*intercept_current - 1d0
+
+        ! Set Initial Value
+        call multi_mat_vec(tmp_x, coefs_current, tmp_r, node_ptr%n_samples, node_ptr%n_columns)
+        tmp_r = tmp_r + intercept_current
+        tmp_sum_l = 0d0
+        tmp_cnt_l = 0_8
+        do j=1, node_ptr % n_samples, 1
+            factor = tmp_r(j) .le. 0d0
+            tmp_sum_l = tmp_sum_l + factor * tmp_y(j,1)
+            tmp_cnt_l = tmp_cnt_l + factor
+        end do
+        tmp_sum_r = tot_sum - tmp_sum_l
+        tmp_cnt_r = tot_cnt - tmp_cnt_l
+        tmp_avg_l = tmp_sum_l / dble(tmp_cnt_l)
+        tmp_avg_r = tmp_sum_r / dble(tmp_cnt_r)
+        gain_current = tmp_cnt_l * tmp_cnt_r / dble(tot_cnt) * (tmp_avg_l - tmp_avg_r)**2d0
+        ! print*, "gain_current: ", gain_current
 
 
+        call random_number(coefs_add)
+        call random_number(intercept_add)
+        coefs_add     = coefs_add - .5d0
+        intercept_add = intercept_add - .5d0
+        coefs_new     = coefs_current     + coefs_add
+        intercept_new = intercept_current + intercept_add
+
+        do e=1, hparam_ptr%max_epoch, 1
+            tmp_r = 0d0
+            call multi_mat_vec(tmp_x, coefs_new, tmp_r, node_ptr%n_samples, node_ptr%n_columns)
+            tmp_r = tmp_r + intercept_new
+            tmp_sum_l = 0d0
+            tmp_cnt_l = 0_8
+
+            do j=1, node_ptr % n_samples, 1
+                factor = tmp_r(j) .le. 0d0
+                tmp_sum_l = tmp_sum_l + factor * tmp_y(j,1)
+                tmp_cnt_l = tmp_cnt_l + factor
+            end do
+            tmp_sum_r = tot_sum - tmp_sum_l
+            tmp_cnt_r = tot_cnt - tmp_cnt_l
+            tmp_avg_l = tmp_sum_l / dble(tmp_cnt_l)
+            tmp_avg_r = tmp_sum_r / dble(tmp_cnt_r)
+            gain_new = tmp_cnt_l * tmp_cnt_r / dble(tot_cnt) * (tmp_avg_l - tmp_avg_r)**2d0
+            gain_diff = gain_current - gain_new
+            ! print*, '============================================================='
+            ! print*, "gain_current: ", gain_current
+            ! print*, "gain_new    : ", gain_new
+            ! print*, "gain_diff   : ", gain_diff
+            ! print*, "       Left : ", tmp_sum_l, tmp_cnt_l, tmp_avg_l
+            ! print*, "       RIght: ", tmp_sum_r, tmp_cnt_r, tmp_avg_r
+            if ( gain_new .gt. gain_current ) then
+                gain_current = gain_new
+                coefs_current = coefs_new
+                intercept_current = intercept_new
+                sum_current_l = tmp_sum_l
+                sum_current_r = tmp_sum_r
+                avg_current_l = tmp_avg_l
+                avg_current_r = tmp_avg_r
+                cnt_current_l = tmp_cnt_l
+                cnt_current_r = tmp_cnt_r
+                ! if (abs(1d0 - gain_diff/gain_current) .ge. 1d0) then
+                !     temperature = temperature * 0.99
+                ! else
+                !     temperature = temperature * abs(1d0 - gain_diff/gain_current)
+                ! end if
+                count_eval = count_eval + 1
+            else
+                transition_proba = minval( (/ exp(-gain_diff/temperature), 1d0 /) )
+                ! print*, "transition_proba: ", transition_proba, gain_diff, temperature, initial_temperature
+                call random_number(proba)
+                if ( proba .lt. transition_proba ) then
+                    gain_current = gain_new
+                    coefs_current = coefs_new
+                    intercept_current = intercept_new
+                    sum_current_l = tmp_sum_l
+                    sum_current_r = tmp_sum_r
+                    avg_current_l = tmp_avg_l
+                    avg_current_r = tmp_avg_r
+                    cnt_current_l = tmp_cnt_l
+                    cnt_current_r = tmp_cnt_r
+                    ! if (abs(1d0 - gain_diff/gain_current) .ge. 1d0) then
+                    !     temperature = temperature * 0.99
+                    ! else
+                    !     temperature = temperature * abs(1d0 - gain_diff/gain_current)
+                    ! end if
+                end if
+                count_eval = count_eval + 1
+            end if
+            temperature = temperature / log(e+0d0)
+
+
+            call random_number(coefs_add)
+            call random_number(intercept_add)
+            coefs_add     = coefs_add - .5d0
+            intercept_add = intercept_add - .5d0
+            coefs_new     = coefs_current     + coefs_add
+            intercept_new = intercept_current + intercept_add
+        end do
+
+        ! print*, '============================================================='
+        ! print*, "gain_current: ", gain_current
+        ! print*, "       Left : ", sum_current_l, cnt_current_l, avg_current_l
+        ! print*, "       RIght: ", sum_current_r, cnt_current_r, avg_current_r
+
+        node_ptr%is_trained = t_
+        node_ptr%eval_counter = count_eval
+        node_ptr%gain_best = gain_current
+
+        if ( count_eval .eq. 0 ) then
+            node_ptr%is_terminal = t_
+            return
+        end if
+
+        allocate(node_ptr%sum_l(node_ptr%n_outputs))
+        allocate(node_ptr%sum_r(node_ptr%n_outputs))
+        allocate(node_ptr%response_l(node_ptr%n_outputs))
+        allocate(node_ptr%response_r(node_ptr%n_outputs))
+
+        node_ptr%coef_ = coefs_current
+        node_ptr%intercept_ = intercept_current
+        node_ptr%threshold_ = 0d0
+        node_ptr%sum_l = sum_current_l
+        node_ptr%sum_r = sum_current_r
+        node_ptr%n_samples_l = cnt_current_l
+        node_ptr%n_samples_r = cnt_current_r
+        node_ptr%response_l = avg_current_l
+        node_ptr%response_r = avg_current_r
+        call node_ptr%hparam_check(hparam_ptr)
+
+        ! print*, "======================================================"
+        ! print*, "Collect: ", tot_time_collect
+        ! print*, "MinMax : ", tot_time_minmax
+        ! print*, "Sum_Up : ", tot_time_sum_up
+    end subroutine split_sadt_regressor_indivisuals
+
+
+    !------------------------------------------------------------------------------------------------------------------------------------------ 
+    !------------------------------------------------------------------------------------------------------------------------------------------ 
     !> A subroutine to split node by very tradisional way.
     !> L. Breiman, J. Friedman, R. Olshen, and C. Stone, “Classification and Regression Trees”, Wadsworth, Belmont, CA, 1984.
     subroutine split_decision_tree_regressor(this, node_ptrs, data_holder_ptr, hparam_ptr, & 
@@ -76,7 +327,6 @@ contains
             end do
         end if
     end subroutine split_decision_tree_regressor
-
 
     !> A subroutine to split node by very tradisional way.
     !> L. Breiman, J. Friedman, R. Olshen, and C. Stone, “Classification and Regression Trees”, Wadsworth, Belmont, CA, 1984.
@@ -237,6 +487,8 @@ contains
     end subroutine split_decision_tree_regressor_indivisuals
 
 
+    !------------------------------------------------------------------------------------------------------------------------------------------ 
+    !------------------------------------------------------------------------------------------------------------------------------------------ 
     !> A subroutine to split node by extremely randomized way.
     subroutine split_extra_tree_regressor(this, node_ptrs, data_holder_ptr, hparam_ptr, & 
         n_columns, feature_indices, feature_indices_scanning_range, is_permute_per_node)
@@ -261,7 +513,6 @@ contains
             end do
         end if
     end subroutine split_extra_tree_regressor
-
 
     !> A subroutine to split node by extremely randomized way.
     subroutine split_extra_tree_regressor_indivisuals(this, node_ptr, data_holder_ptr, hparam_ptr, &
@@ -437,6 +688,8 @@ contains
     end subroutine split_extra_tree_regressor_indivisuals
 
 
+    !------------------------------------------------------------------------------------------------------------------------------------------ 
+    !------------------------------------------------------------------------------------------------------------------------------------------ 
     subroutine split_extra_tree_regressor_faster(this, node_ptrs, data_holder_ptr, hparam_ptr, & 
         n_columns, feature_indices, feature_indices_scanning_range, is_permute_per_node)
         implicit none
@@ -460,7 +713,6 @@ contains
             end do
         end if
     end subroutine split_extra_tree_regressor_faster
-
 
     !> A subroutine to split node by extremely randomized way.
     subroutine split_extra_tree_regressor_faster_indivisuals(this, node_ptr, data_holder_ptr, hparam_ptr, &
@@ -624,6 +876,8 @@ contains
     end subroutine split_extra_tree_regressor_faster_indivisuals
 
 
+    !------------------------------------------------------------------------------------------------------------------------------------------ 
+    !------------------------------------------------------------------------------------------------------------------------------------------ 
     !> A subroutine to split node by 'clouds'.
     subroutine split_clouds_regressor(this, node_ptrs, data_holder_ptr, hparam_ptr, &
         n_columns, feature_indices, feature_indices_scanning_range, is_permute_per_node)
@@ -648,7 +902,6 @@ contains
             end do
         end if
     end subroutine split_clouds_regressor
-
 
     !> A subroutine to split node by 'clouds_regressor'.
     subroutine split_clouds_regressor_indivisuals(this, node_ptr, data_holder_ptr, hparam_ptr, &
@@ -862,6 +1115,9 @@ contains
         ! print*, node_ptr%n_samples, num_l, num_r
     end subroutine split_clouds_regressor_indivisuals
 
+
+    !------------------------------------------------------------------------------------------------------------------------------------------ 
+    !------------------------------------------------------------------------------------------------------------------------------------------ 
     !> A subroutine to split node by 'clouds'.
     subroutine split_weighted_oblique_decision_tree_classifier(this, node_ptrs, data_holder_ptr, hparam_ptr, &
         n_columns, feature_indices, feature_indices_scanning_range, is_permute_per_node)
@@ -887,7 +1143,6 @@ contains
             end do
         end if
     end subroutine split_weighted_oblique_decision_tree_classifier
-
 
     !> A subroutine to split node by 'clouds_regressor'.
     subroutine split_weighted_oblique_decision_tree_classifier_indivisuals(this, node_ptr, data_holder_ptr, hparam_ptr, &
