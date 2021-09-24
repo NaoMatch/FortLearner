61926b06cb6a9204313bc5c0834b14b8460c7e84
NaoMatch <mn1491625@gmail.com>
2021-09-24 22:50:52 +0900

    change variable name from 'uniq_v' to 'uniq_vals'

--- common/include/common/collect_unique_values/inc_collect_unique_values_detail.f90
+++ common/include/common/collect_unique_values/inc_collect_unique_values_detail.f90
@@ -1,11 +1,11 @@
-if (allocated(uniq_v)) deallocate(uniq_v)
+if (allocated(uniq_vals)) deallocate(uniq_vals)
 idx = 2
 n_unique = count_unique(vector, n_samples)
-allocate(uniq_v(n_unique))
-uniq_v(1) = vector(1)
+allocate(uniq_vals(n_unique))
+uniq_vals(1) = vector(1)
 do n=2, n_samples
     if ( vector(n-1) .ne. vector(n) ) then
-        uniq_v(idx) = vector(n)
+        uniq_vals(idx) = vector(n)
         idx = idx + 1
     end if
 end do
