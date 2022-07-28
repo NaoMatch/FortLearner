79b3c9f5388d357294d7e09d24245e00ae900e4a
NaoMatch <mn1491625@gmail.com>
2022-07-28 23:50:49 +0900

    add new dataset for nearest neighbor search

--- test_script/create_datasets.py
+++ test_script/create_datasets.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.datasets import make_regression, make_blobs
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import DBSCAN
+from sklearn.neighbors import KDTree
 
 n_samples_list = [1000, 100000]
 n_columns_list = [5, 100]
@@ -27,3 +28,22 @@ pd.DataFrame(X).to_csv("../sample_data/make_brobs_X_750x2.csv", index=False)
 pd.DataFrame(labels_true).to_csv("../sample_data/make_brobs_Y_750x2.csv", index=False)
 db = DBSCAN(eps=0.25, min_samples=8).fit(X)
 pd.DataFrame([x+1 for x in db.labels_]).to_csv("../sample_data/make_brobs_DBSCAN_750x2.csv", index=False)
+
+
+print("Create Nearest Neighbor Search Datasets")
+n_samples_list = [100000 + 100] # Sample + Query
+n_columns_list = [100]
+for n_samples in n_samples_list:
+    for n_columns in n_columns_list:
+        print("     #Samples={}, #Columns={}".format(n_samples, n_columns))
+        X, _ = make_regression(n_samples=n_samples, n_features=n_columns)
+        
+        pd.DataFrame(X[:-100,:]).to_csv("../sample_data/nnsearch_X_{0:0=10}x{1:0=5}.csv".format(len(X[:-100,:]), n_columns), index=False)
+        pd.DataFrame(X[-100:,:]).to_csv("../sample_data/nnsearch_Q_{0:0=10}x{1:0=5}.csv".format(len(X[-100:,:]), n_columns), index=False)
+
+        tree = KDTree(X[:-100,:], leaf_size=32)
+        dst, ind = tree.query(X[-100:,:], k=100000)
+
+        ind = ind + 1
+        pd.DataFrame(dst).to_csv("../sample_data/nnsearch_D_{0:0=10}x{1:0=5}.csv".format(100, 100000), index=False)
+        pd.DataFrame(ind).to_csv("../sample_data/nnsearch_I_{0:0=10}x{1:0=5}.csv".format(100, 100000), index=False)
--- test_script/main_csv2bin.f90
+++ test_script/main_csv2bin.f90
@@ -5,14 +5,24 @@ program main_csv2bin
     integer(kind=8) :: date_value1(8), date_value2(8), time_dt, time_et, time_cl, time_lw
 
     integer(kind=8)    :: n_samples_trains(5), n_columns_trains(5)
+
     integer(kind=8)    :: n_samples_train, n_columns_train 
     integer(kind=8)    :: n_samples_test, n_columns_test
     logical(kind=4)    :: skip_header
     CHARACTER(len=1)   :: dtype_in, dtype_out
     CHARACTER(len=256) :: file_name_x_train_csv, file_name_y_train_csv
     CHARACTER(len=256) :: file_name_x_train_bin, file_name_y_train_bin
-    CHARACTER(len=256) :: file_name_x_test_csv, file_name_y_test_csv
-    CHARACTER(len=256) :: file_name_x_test_bin, file_name_y_test_bin
+    
+    integer(kind=8)    :: n_samples_x, n_columns_x
+    integer(kind=8)    :: n_samples_q, n_columns_q
+    CHARACTER(len=256) :: file_name_x_csv, file_name_x_bin
+    CHARACTER(len=256) :: file_name_q_csv, file_name_q_bin
+        
+    integer(kind=8)    :: n_samples_d, n_columns_d
+    integer(kind=8)    :: n_samples_i, n_columns_i
+    CHARACTER(len=256) :: file_name_d_csv, file_name_d_bin
+    CHARACTER(len=256) :: file_name_i_csv, file_name_i_bin
+
     real(kind=8), ALLOCATABLE :: x_train(:,:)
     real(kind=8), ALLOCATABLE :: y_train(:,:)
     real(kind=8), ALLOCATABLE :: y_train_pred(:,:)
@@ -38,7 +48,72 @@ program main_csv2bin
     call read2bin_2d(file_name_y_train_csv, file_name_y_train_bin, &
         n_samples_train, 1_8, skip_header, dtype_in, dtype_out)
 
-        print*, '============================================================='
+    print*, '============================================================='
+    print*, '============================================================='
+    file_name_x_train_csv = "../sample_data/make_regression_X_0000100000x00100.csv"
+    file_name_y_train_csv = "../sample_data/make_regression_y_0000100000x00100.csv"
+    file_name_x_train_bin = "../sample_data/make_regression_X_0000100000x00100.bin"
+    file_name_y_train_bin = "../sample_data/make_regression_y_0000100000x00100.bin"
+    n_samples_train = 100000
+    n_columns_train = 100
+    skip_header = t_
+    dtype_in  = "r"
+    dtype_out = "r"
+
+    print*, "CSV to Binary"
+    print*, "    x_train"
+    call read2bin_2d(file_name_x_train_csv, file_name_x_train_bin, &
+        n_samples_train, n_columns_train, skip_header, dtype_in, dtype_out)
+    print*, "    y_train"
+    call read2bin_2d(file_name_y_train_csv, file_name_y_train_bin, &
+        n_samples_train, 1_8, skip_header, dtype_in, dtype_out)
+
+    print*, '============================================================='
+    print*, '============================================================='
+    file_name_x_csv = "../sample_data/nnsearch_X_0000100000x00100.csv"
+    file_name_q_csv = "../sample_data/nnsearch_Q_0000000100x00100.csv"
+    file_name_x_bin = "../sample_data/nnsearch_X_0000100000x00100.bin"
+    file_name_q_bin = "../sample_data/nnsearch_Q_0000000100x00100.bin"
+    n_samples_x = 100000
+    n_columns_x = 100
+    n_samples_q = 100
+    n_columns_q = 100
+    skip_header = t_
+    dtype_in  = "r"
+    dtype_out = "r"
+
+    print*, "CSV to Binary"
+    print*, "    x"
+    call read2bin_2d(file_name_x_csv, file_name_x_bin, &
+        n_samples_x, n_columns_x, skip_header, dtype_in, dtype_out)
+    print*, "    q"
+    call read2bin_2d(file_name_q_csv, file_name_q_bin, &
+        n_samples_q, n_columns_q, skip_header, dtype_in, dtype_out)
+
+
+    print*, '============================================================='
+    print*, '============================================================='
+    file_name_d_csv = "../sample_data/nnsearch_D_0000000100x100000.csv"
+    file_name_i_csv = "../sample_data/nnsearch_I_0000000100x100000.csv"
+    file_name_d_bin = "../sample_data/nnsearch_D_0000000100x100000.bin"
+    file_name_i_bin = "../sample_data/nnsearch_I_0000000100x100000.bin"
+    n_samples_d = 100
+    n_columns_d = 100000
+    n_samples_i = 100
+    n_columns_i = 100000
+    skip_header = t_
+    dtype_in  = "r"
+    dtype_out = "r"
+
+    print*, "CSV to Binary"
+    print*, "    x_train"
+    call read2bin_2d(file_name_d_csv, file_name_d_bin, &
+        n_samples_d, n_columns_d, skip_header, dtype_in, dtype_out)
+    print*, "    y_train"
+    call read2bin_2d(file_name_i_csv, file_name_i_bin, &
+        n_samples_i, n_columns_i, skip_header, "i", "i")
+
+    print*, '============================================================='
     print*, '============================================================='
     file_name_x_train_csv = "../sample_data/make_brobs_X_750x2.csv"
     file_name_y_train_csv = "../sample_data/make_brobs_DBSCAN_750x2.csv"
