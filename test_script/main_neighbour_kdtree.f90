b8eb497ef5ab07c46c186edeff5df9baf5ccb744
NaoMatch <mn1491625@gmail.com>
2022-07-28 23:50:02 +0900

    update. compare sklearn result

--- test_script/main_neighbour_kdtree.f90
+++ test_script/main_neighbour_kdtree.f90
@@ -1,54 +1,73 @@
 program main_neighbour_kdtree
+    use mod_common
     use mod_nearest_neighbour
+    use mod_brute_force_search
     use mod_kdtree
     implicit none
 
-    integer(kind=8) :: n_samples_x, n_samples_q
-    integer(kind=8) :: n_columns
+    type(brute_force_search) :: bfsearch
+    type(kdtree) :: kdtsearch
+    type(neighbor_results) :: res
 
-    real(kind=8), allocatable :: x(:,:), q(:,:)
-    type(kdtree) :: ktree, ktree2
-    type(neighbor_results)   :: res, res2
+    real(kind=8), allocatable :: x(:,:)
+    real(kind=8), allocatable :: q(:,:)
+    real(kind=8), allocatable :: d(:,:)
+    integer(kind=8), allocatable :: i(:,:), i_pack_sk(:), i_pack_fl(:)
+    integer(kind=8) :: n, counter
+    integer(kind=8) :: n_x, n_max
+    real(kind=8)    :: radius
 
-    n_samples_x = 1000
-    n_samples_q = 10
-    n_columns   = 10
+    character(len=256) :: fn_x
+    character(len=256) :: fn_q
+    character(len=256) :: fn_d
+    character(len=256) :: fn_i
 
-    allocate(x(n_samples_x, n_columns))
-    allocate(q(n_samples_q, n_columns))
+    fn_x = "../sample_data/nnsearch_X_0000100000x00100.bin"
+    fn_q = "../sample_data/nnsearch_Q_0000000100x00100.bin"
+    fn_d = "../sample_data/nnsearch_D_0000000100x100000.bin"
+    fn_i = "../sample_data/nnsearch_I_0000000100x100000.bin"
 
-    call random_number(x)
-    call random_number(q)
+    call read_bin_2d(fn_x, x)
+    call read_bin_2d(fn_q, q)
+    call read_bin_2d(fn_d, d)
+    call read_bin_2d(fn_i, i)
 
+    n_x = size(x, dim=1)
+    n_x = 1000
+    radius = 20d0
 
-    ktree = kdtree()
-    print*, "    build start."
-    call ktree%build(x)
-    print*, "    build done."
-    
+    kdtsearch = kdtree(min_samples_in_leaf=64_8)
+    call kdtsearch%build(x)
+    res = kdtsearch%query(q, n_neighbors=n_x)
+    do n=1, size(q, dim=1), 1
+        ! print*, '*********************************************************************************************'
+        ! print*, i(n,:n_x)
+        ! print*, res%indices(n)%idx(:n_x)
+        ! print*, res%distances(n)%dst(:n_x)
+        counter = count(i(n,:n_x) == res%indices(n)%idx(:n_x))
+        if (counter /= n_x) then
+            stop "n_neighbors: Result Mismatch!"
+        end if
+    end do
+    print*, "n_neighbors: Success!"
 
-    print*, "    dump start."
-    call ktree%dump(file_name="ktree.bin")
-    print*, "    dump done."
-
-
-    print*, "    load start."
-    call ktree2%load(file_name="ktree.bin")
-    print*, "    load done."
-
-    
-    print*, '*********************************************************************************************'
-    print*, '*********************************************************************************************'
-    res = ktree%query(q, radius=.8d0)
-    res2 = ktree2%query(q, radius=.8d0)
-    print*, res%distances(1)%dst(:)
-    print*, res2%distances(1)%dst(:)
-
-    print*, '*********************************************************************************************'
-    print*, '*********************************************************************************************'
-    res = ktree%query(q, n_neighbors=10_8)
-    res2 = ktree2%query(q, n_neighbors=10_8)
-    print*, res%distances(1)%dst(:)
-    print*, res2%distances(1)%dst(:)
-
+    kdtsearch = kdtree(min_samples_in_leaf=64_8)
+    call kdtsearch%build(x)
+    res = kdtsearch%query(q, radius=radius)
+    do n=1, size(q, dim=1), 1
+        i_pack_sk = pack(i(n,:), mask=d(n,:)<=radius)
+        i_pack_fl = res%indices(n)%idx(:)
+        ! n_max = minval([size(i_pack_sk)+0_8, 10_8])
+        ! print*, '*********************************************************************************************'
+        ! print*, size(i_pack_sk), size(i_pack_fl)
+        ! print*, int(i_pack_sk(:n_max))
+        ! print*, int(res%indices(n)%idx(:n_max))
+        ! print*, real(d(n, :n_max))
+        ! print*, real(res%distances(n)%dst(:n_max))
+        counter = count(i_pack_sk == i_pack_fl)
+        if (counter /= size(i_pack_sk)) then
+            stop "radius: Result Mismatch!"
+        end if
+    end do
+    print*, "radius: Success!"
 end program main_neighbour_kdtree
