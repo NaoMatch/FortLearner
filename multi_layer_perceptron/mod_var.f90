8a324a604058df5d61a15975cf20d9be632efc9a
NaoMatch <mn1491625@gmail.com>
2022-08-07 13:51:42 +0900

    wip

--- multi_layer_perceptron/mod_var.f90
+++ multi_layer_perceptron/mod_var.f90
@@ -1,15 +1,31 @@
 module mod_var
-    ! 型が一緒であれば継承元の関数をオーバーライドできる
+    ! use mod_activation_functions2
     implicit none
-    
+
     type variable
         real(kind=8), allocatable :: v(:,:)
+        real(kind=8), allocatable :: g(:,:)
+        type(activation_function), pointer :: creator_ptr
+        class(*), pointer :: creator_tmp
+    contains
+        procedure :: backward => backward_variable
     end type variable
 
     interface variable
         module procedure :: new_variable_s
         module procedure :: new_variable_m
     end interface variable
+
+    type activation_function
+        character(len=256) :: act_name
+        type(variable), pointer :: input_ptr
+    contains
+        ! Forward
+        procedure :: square
+        procedure :: exponential
+        ! Backward
+        procedure :: backward => backward_activation_funcion
+    end type activation_function
 
 contains
 
@@ -18,6 +34,7 @@ contains
         type(variable) :: new_variable_s
         real(kind=8), intent(in) :: sclr
         new_variable_s%v = reshape( (/sclr/), shape = [1,1] )
+        nullify(new_variable_s%creator_ptr)
     end function new_variable_s
     
     function new_variable_m(mtrx)
@@ -25,6 +42,51 @@ contains
         type(variable) :: new_variable_m
         real(kind=8), intent(in) :: mtrx(:,:)
         new_variable_m%v = mtrx(:,:)
+        nullify(new_variable_m%creator_ptr)
     end function new_variable_m
+
+    recursive subroutine backward_variable(this)
+        implicit none
+        class(variable) :: this
+        type(variable) :: var
+        if (associated(this%creator_ptr)) then
+            this%creator_ptr%input_ptr%g = this%creator_ptr%backward(this%g)
+            call this%creator_ptr%input_ptr%backward()
+        end if
+    end subroutine backward_variable
+
+    function square(this, input_var) result(output_var)
+        implicit none
+        class(activation_function), target :: this
+        type(variable), target :: input_var
+        type(variable) :: output_var
+        this%act_name = "square"
+        output_var%creator_ptr => this
+        output_var%creator_ptr%input_ptr => input_var
+        output_var%v = input_var%v**2d0
+    end function square
     
+    function exponential(this, input_var) result(output_var)
+        implicit none
+        class(activation_function), target :: this
+        type(variable), target :: input_var
+        type(variable) :: output_var
+        this%act_name = "exponential"
+        output_var%creator_ptr => this
+        output_var%creator_ptr%input_ptr => input_var
+        output_var%v = exp(input_var%v)
+    end function exponential
+
+    function backward_activation_funcion(this, grad_in) result(grad_out)
+        implicit none
+        class(activation_function) :: this
+        real(kind=8), intent(in) :: grad_in(:,:)
+        real(kind=8), allocatable :: grad_out(:,:)
+        if (this%act_name == "square") then
+            grad_out = 2d0 * this%input_ptr%v(:,:) * grad_in(:,:)
+        elseif (this%act_name == "exponential") then
+            grad_out = exp(this%input_ptr%v(:,:)) * grad_in(:,:)
+        end if
+    end function backward_activation_funcion
+
 end module mod_var
--- test_script/main_multi_layer_perceptron.f90
+++ test_script/main_multi_layer_perceptron.f90
@@ -1,28 +1,30 @@
 program main_multi_layer_perceptron
     use mod_var
-    use mod_func
+    use mod_activation_functions2
     implicit none
 
-    type(func) :: f
-    type(square) :: sq
-    type(exponential) :: ex
-    type(sub) :: sb
-    type(variable) :: var_x, var_m
-    real(kind=8) :: x
-    real(kind=8), allocatable :: v(:), vm(:,:)
-    real(kind=8), allocatable :: m(:,:)
+    type(variable) :: var_x, var_a, var_b, var_c, var_d, grad
+    real(kind=8) :: x(1,1), g(1,1)
+    type(activation_function) :: act1, act2, act3
+    integer(kind=8) :: iter, max_iter
 
+    max_iter = 20
     x = .5d0
-    var_x = variable(x)
+    g = 1d0
+    
+    x = .5d0
+    max_iter = 100
+    do iter=1, max_iter, 1
+        var_x = variable(x)
+        var_a = act1%square(var_x)
+        var_b = act2%exponential(var_a)
+        var_c = act3%square(var_b)
+        
+        var_c%g = g
+        call var_c%backward()
+        x = var_x%v - 0.1d0 * var_x%g
+    end do
+    print*, x
 
-    var_x = sq%act(var_x)
-    print*, var_x%v
-    var_x = ex%act(var_x)
-    print*, var_x%v
-    var_x = sq%act(var_x)
-    print*, var_x%v
-
-
-    var_x = sb%act(var_x)
 
 end program main_multi_layer_perceptron
