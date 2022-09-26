591debbca6b9a36d87a5879b5150939cc52def58
NaoMatch <mn1491625@gmail.com>
2022-09-26 21:31:56 +0900

    set new argument, double_weight

--- multi_layer_perceptron/mod_mnist_classifier.f90
+++ multi_layer_perceptron/mod_mnist_classifier.f90
@@ -6,8 +6,8 @@ module mod_mnist_classifier
     implicit none
 
     type, extends(neural_network) :: mnist_classifier
-        type(dense) :: dense1 = dense(784_8, 100_8, bias=t_)
-        type(dense) :: dense2 = dense(100_8,  10_8, bias=t_)
+        type(dense) :: dense1 = dense(784_8, 100_8, bias=t_, double_weight=f_)
+        type(dense) :: dense2 = dense(100_8,  10_8, bias=t_, double_weight=f_)
     contains
         procedure :: forward => forward_mnist_classifier
     end type mnist_classifier
@@ -38,8 +38,7 @@ contains
 
         h = this%dense1%act(input_var)
         h = relu(h)
-        h = this%dense2%act(h)
-        output_var = sigmoid(h)
+        output_var = this%dense2%act(h)
 
         ! Postprocess
         call this%postprocess()
