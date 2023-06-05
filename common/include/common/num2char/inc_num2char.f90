15c51acbdb8c24e060c46efc4ab304e1b0d594fa
NaoMatch <mn1491625@gmail.com>
2023-06-05 21:08:55 +0900

    real用の数値から文字列へ変換する関数を修正

--- common/include/common/num2char/inc_num2char.f90
+++ common/include/common/num2char/inc_num2char.f90
@@ -1,13 +1,3 @@
-function num2char_r8(num)
-    character(:), allocatable   :: num2char_r8
-    real(kind=8), intent(in) :: num
-    integer(kind=8) :: num_digit
-
-    allocate(character(10)::num2char_r8)
-    write (num2char_r8, '(E10.3e2)') num
-end function num2char_r8
-
-
 function num2char_i4(num)
     character(:), allocatable   :: num2char_i4
     integer(kind=4), intent(in) :: num
@@ -28,3 +18,21 @@ function num2char_i8(num)
     allocate(character(num_digit)::num2char_i8)
     write (num2char_i8, '(i0)') num
 end function num2char_i8
+
+function num2char_r8(num) result(char)
+    implicit none
+    real(kind=8), intent(in) :: num
+    character(:), allocatable :: char
+    character(20) :: tmp
+    character(:), allocatable :: char_digit
+    integer(kind=8) :: digit
+    character(1) :: sign
+
+    digit = get_real_digit(abs(num))
+    char_digit = num2char(digit)
+
+    write (tmp, '(f20.18)') abs(num) * 10d0**(-digit)
+    sign = " "
+    if (num<0d0) sign = "-"
+    char = sign // tmp // "d" // char_digit
+end function num2char_r8
