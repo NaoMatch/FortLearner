8e7f409dbabc376bb1e50fbe7808f41302b012e2
NaoMatch <mn1491625@gmail.com>
2023-06-27 00:50:24 +0900

    implement 'one_at_a_time_hash' for real(scalar, vector, matrix)

--- common/mod_hash.f90
+++ common/mod_hash.f90
@@ -6,6 +6,9 @@ module mod_hash
         module procedure :: one_at_a_time_hash_i8
         module procedure :: one_at_a_time_hash_vec_i8
         module procedure :: one_at_a_time_hash_mat_i8
+        module procedure :: one_at_a_time_hash_r8
+        module procedure :: one_at_a_time_hash_vec_r8
+        module procedure :: one_at_a_time_hash_mat_r8            
     end interface one_at_a_time_hash
     
     interface xorshift64_hash
@@ -15,13 +18,84 @@ module mod_hash
 
 contains
 
+    function one_at_a_time_hash_r8(val)
+        implicit none
+        real(kind=8), intent(in) :: val
+        integer(kind=8) :: one_at_a_time_hash_r8
+
+        integer(kind=8) :: val_i8
+        val_i8 = transfer(val, 0_8)
+
+        one_at_a_time_hash_r8 = one_at_a_time_hash_i8(val_i8)
+    end function one_at_a_time_hash_r8
+
+    function one_at_a_time_hash_vec_r8(vector, n_elements)
+        implicit none
+        real(kind=8), intent(in) :: vector(n_elements)
+        integer(kind=8), intent(in) :: n_elements
+        integer(kind=8) :: one_at_a_time_hash_vec_r8
+
+        integer(kind=8) :: hash
+        integer(kind=8) :: i
+
+        hash = 0_8
+
+        do i=1, n_elements, 1
+            print*, transfer(vector(i), 0_8)
+            hash = hash + transfer(vector(i), 0_8)
+            hash = hash + ishft(hash, 10_8)
+            hash = xor(hash, ishft(hash, -6_8))
+        end do
+        hash = hash + ishft(hash, 3_8)
+        hash = xor(hash, ishft(hash, -11_8))
+        hash = hash + ishft(hash, 15_8)
+
+        one_at_a_time_hash_vec_r8 = hash
+    end function one_at_a_time_hash_vec_r8
+
+    function one_at_a_time_hash_mat_r8(matrix, n_samples, n_elements)
+        implicit none
+        real(kind=8), intent(in)  :: matrix(n_samples, n_elements)
+        integer(kind=8), intent(in)  :: n_samples, n_elements
+        integer(kind=8), ALLOCATABLE, target :: one_at_a_time_hash_mat_r8(:)
+        integer(kind=8), pointer :: res_ptr(:)
+
+        integer(kind=8) :: i, j, jj, hash
+        integer(kind=8) :: j_unroll, j_remain, j_unroll_size
+        integer(kind=8) :: buffer_hash(15)
+
+        allocate( one_at_a_time_hash_mat_r8(n_samples) )
+        one_at_a_time_hash_mat_r8(:) = 0_8
+
+        res_ptr => one_at_a_time_hash_mat_r8
+
+        do j=1, n_elements, 1
+            do i=1, n_samples, 1
+                print*, transfer(matrix(i,j), 0_8)
+                hash = one_at_a_time_hash_mat_r8(i)
+                hash = hash + transfer(matrix(i,j), 0_8)
+                hash = hash + ishft(hash, 10_8)
+                hash = xor(hash, ishft(hash, -6_8))
+                one_at_a_time_hash_mat_r8(i) = hash
+            end do
+        end do
+
+        do i=1, n_samples, 1
+            hash = one_at_a_time_hash_mat_r8(i)
+            hash = hash + ishft(hash, 3_8)
+            hash = xor(hash, ishft(hash, -11_8))
+            hash = hash + ishft(hash, 15_8)
+            one_at_a_time_hash_mat_r8(i) = hash
+        end do
+    end function one_at_a_time_hash_mat_r8
+
+
     function one_at_a_time_hash_i8(val)
         implicit none
         integer(kind=8), intent(in) :: val
         integer(kind=8) :: one_at_a_time_hash_i8
 
         integer(kind=8) :: hash
-        integer(kind=8) :: i
 
         hash = 0_8
 
