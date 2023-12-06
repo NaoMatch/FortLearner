cb6a7cb09e13147cb3fc985b570cafc607532b1f
NaoMatch <mn1491625@gmail.com>
2023-12-06 21:41:29 +0900

    replace user defined matrix vector multiplication to dgemv

--- work/kdtree/main.f90
+++ work/kdtree/main.f90
@@ -3,11 +3,12 @@ program main
     use mod_timer
     use mod_linalg
     use mod_nearest_neighbour
+    use mod_kdtree
     implicit none
     integer(kind=8) :: date_value1(8), date_value2(8)
     integer(kind=8), allocatable :: time1(:), time2(:)
     type(kdtree) :: tree, tree_pointer
-    type(kdtree_results) :: results
+    type(neighbor_results) :: results, results2
     
     real(kind=8), ALLOCATABLE, target :: x(:,:), x_sq_sum(:), dist(:,:)
     real(kind=8), ALLOCATABLE, target :: y(:,:), y_sq_sum(:)
@@ -17,6 +18,7 @@ program main
 
     integer(kind=8) :: n_rows_x, n_rows_y, n_cols, i, j, n, iter, n_iter, count_eq, flg, n_neighbors, pow_
     integer(kind=8) :: count_dup
+    integer(kind=8) :: time_1, time_2
 
     integer(kind=8) :: idx, fid
     real(kind=8) :: val, diff
@@ -25,8 +27,8 @@ program main
     n_cols = 32_8
     allocate(x(n_rows_x, n_cols))
     call RANDOM_NUMBER(x)
-    x = int(10d0*x-10d0*.5d0)
-    tree_pointer = kdtree(min_samples_in_leaf=128_8)
+    x = 10d0*x-10d0*.5d0
+    tree_pointer = kdtree(min_samples_in_leaf=256_8)
 
     call date_and_time(values=date_value1)
     call tree_pointer%build(x)
@@ -36,22 +38,19 @@ program main
     do i=0, 3, 1
         n_rows_y = 10**i
         call date_and_time(values=date_value1)
-        ! results = tree_pointer%query(x(1:n_rows_y,:), radius=0d0)
-        results = tree_pointer%query(x(1:n_rows_y,:), n_neighbors=3_8)
-        call date_and_time(values=date_value2)
-
-        count_dup = 0
-        do n=1, n_rows_y, 1
-            ! if ( size(results%indices(n)%idx) .ge. 1_8 ) then
-            !     count_dup = count_dup + 1
-            ! end if
-            print*, n, size(results%indices(n)%idx), results%distances(n)%dst(:)
+        do j=1, 1, 1
+            results = tree_pointer%query(x(1:n_rows_y,:), n_neighbors=16_8)
         end do
-
-        print*, "QueryTree:                  ", n_rows_y, count_dup, time_diff(date_value1, date_value2)
+        call date_and_time(values=date_value2)
+        time_1 = time_diff(date_value1, date_value2)
+        print*, "QueryTree:                  ", n_rows_y, time_1
     end do
 
-
+    do i=1, 10
+        print*, '*********************************************************************************************'
+        print*, results%indices(i)%idx(1:5)
+        print*, results2%indices(i)%idx(1:5)
+    end do
 
 
     ! do n_cols=5, 100, 5
