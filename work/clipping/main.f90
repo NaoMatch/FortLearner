44b815ff6b1c3356b1b41d294faf045f2b783c23
NaoMatch <mn1491625@gmail.com>
2024-05-05 09:43:07 +0900

    add new functions, clipping lower and upper side

--- work/clipping/main.f90
+++ work/clipping/main.f90
@@ -3,7 +3,7 @@ program main
     implicit none
 
     real(kind=8) :: val, min_val, max_val
-    real(kind=8), allocatable :: x(:)
+    real(kind=8), allocatable :: x(:), x_copy(:)
 
     min_val = 0d0
     max_val = 10d0 
@@ -21,13 +21,32 @@ program main
     print*, val, min_val, max_val , " -> ", clipping(val, min_val, max_val)
 
 
-    allocate(x(10))
+    allocate(x(10), x_copy(10))
     call random_number(x)
-    print*, x
-    min_val = 0d0
+    x = 2d0 * x - 1d0
+    min_val = .2d0
     max_val = .5d0
-    call clipping_array(x, 10_8, min_val, max_val)
-    print*, x
+
+    print*, '*********************************************************************************************'
+    print*, "Clip both side"
+    x_copy = x
+    call clipping_array(x_copy, 10_8, min_val, max_val)
+    print*, real(x)
+    print*, real(x_copy)
+
+    print*, '*********************************************************************************************'
+    print*, "Clip from below"
+    x_copy = x
+    call clipping_array_lower(x_copy, 10_8, min_val)
+    print*, real(x)
+    print*, real(x_copy)
+
+    print*, '*********************************************************************************************'
+    print*, "Clip from upper"
+    x_copy = x
+    call clipping_array_upper(x_copy, 10_8, max_val)
+    print*, real(x)
+    print*, real(x_copy)
 
 
 
