31aab90318b31e4d9791d56b0f67001a60a462dc
NaoMatch <mn1491625@gmail.com>
2025-09-25 22:21:28 +0900

    update

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import subprocess
 from pathlib import Path
 from setuptools import setup
 from setuptools.command.build_py import build_py as _build_py
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
 
 class build_py(_build_py):
@@ -26,5 +27,12 @@ class build_py(_build_py):
         super().run()
 
 
-setup(cmdclass={"build_py": build_py})
+class bdist_wheel(_bdist_wheel):
+    """Mark wheel as platform-specific so bundled libs are kept."""
 
+    def finalize_options(self):
+        super().finalize_options()
+        self.root_is_pure = False
+
+
+setup(cmdclass={"build_py": build_py, "bdist_wheel": bdist_wheel})
